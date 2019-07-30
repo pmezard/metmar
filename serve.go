42cec662a52f20eca7f59d41e62958db0595ceab
Patrick Mezard <patrick@mezard.eu>
2019-07-30 09:05:17 +0200

    serve: update meteofrance scraper
    They decided to move from a json api to returning html fragments. I
    suppose this is called progress...

--- serve.go
+++ serve.go
@@ -4,171 +4,117 @@ import (
 	"bytes"
 	"crypto/sha256"
 	"encoding/hex"
-	"encoding/json"
 	"fmt"
 	"html/template"
+	"io"
 	"net/http"
 	"path"
 	"strings"
 
-	"github.com/daaku/go.httpgzip"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/transform"
+
+	"github.com/PuerkitoBio/goquery"
+	httpgzip "github.com/daaku/go.httpgzip"
 )
-
-type Region struct {
-	Situation   string `json:"situation"`
-	Observation string `json:"observation"`
-	WindAndSea  string `json:"ventEtMer"`
-	Waves       string `json:"houle"`
-	Visibility  string `json:"visi"`
-	Confidence  string `json:"indice"`
-}
-
-type Echeance struct {
-	Title string    `json:"titreEcheance"`
-	Areas []*Region `json:"region"`
-}
-
-type Bulletin struct {
-	Title     string      `json:"titreBulletin"`
-	Special   string      `json:"bulletinSpecial"`
-	Echeances []*Echeance `json:"echeance"`
-}
-
-func formatReport(b *Bulletin) string {
-	buf := &bytes.Buffer{}
-	w := func(format string, args ...interface{}) {
-		fmt.Fprintf(buf, format, args...)
-	}
-	wif := func(s string) {
-		s = strings.TrimSpace(s)
-		if s != "" {
-			s = strings.Replace(s, "<br />", "\n", -1)
-			w("%s\n", strings.TrimRight(s, " \n"))
-		}
-	}
-	w("%s\n", b.Title)
-	w("Bulletin spécial: %s\n", b.Special)
-	w("\n")
-	for _, e := range b.Echeances {
-		w("%s\n", e.Title)
-		for _, r := range e.Areas {
-			wif(r.Situation)
-			wif(r.Observation)
-			parts := strings.Split(r.WindAndSea, "MER :")
-			if len(parts) == 2 {
-				wif(parts[0])
-				wif("MER :" + parts[1])
-			} else {
-				wif(r.WindAndSea)
-			}
-			wif(r.Waves)
-			wif(r.Visibility)
-			wif(r.Confidence)
-		}
-		w("\n")
-	}
-	return buf.String()
-}
 
 func hashReport(report string) string {
 	h := sha256.Sum256([]byte(report))
 	return hex.EncodeToString(h[:])
 }
 
-func getJson(url string, output interface{}) error {
-	rsp, err := http.Get(url)
-	if err != nil {
-		return fmt.Errorf("could not get %s: %s", url, err)
-	}
-	defer rsp.Body.Close()
-	if rsp.StatusCode != 200 {
-		return fmt.Errorf("failed to get %s: %s", url, rsp.Status)
-	}
-	err = json.NewDecoder(rsp.Body).Decode(output)
-	if err != nil {
-		return fmt.Errorf("could not decode json response for %s: %s", url, err)
-	}
-	return nil
-}
-
-func fetchWeather(id string) (string, error) {
-	url := "http://www.meteofrance.com/mf3-rpc-portlet/rest/bulletins/cote/" + id +
-		"/bulletinsMarineMetropole"
-	reports := []*Bulletin{}
-	err := getJson(url, &reports)
-	if err != nil {
-		return "", err
-	}
-	if len(reports) <= 0 {
-		return "", fmt.Errorf("no report retrieved")
-	}
-	return formatReport(reports[0]), nil
-}
-
-func formatJsonWeather(w http.ResponseWriter, req *http.Request) {
-	id := path.Base(req.URL.Path)
-	report, err := fetchWeather(id)
-	w.Header().Set("Content-Type", "text/plain;charset=utf-8")
-	if err != nil {
-		w.WriteHeader(500)
-		fmt.Fprintf(w, "error: %s\n", err)
-		return
-	}
-	h := hashReport(report)
-	w.Header().Set("ETag", h)
-	etag := req.Header.Get("If-None-Match")
-	if etag == h {
-		w.WriteHeader(304)
-		return
-	}
-	fmt.Fprintf(w, "%s", report)
-}
-
-type CoastalArea struct {
-	Id  string
-	URL string
-}
-
-func fetchAreas() ([]CoastalArea, error) {
-	type Area struct {
-		Id  string `json:"id"`
-		URL string `json:"url"`
-	}
-
-	type AreaGroup struct {
-		Name  string `json:"name"`
-		Areas []Area `json:"zones"`
-	}
-
-	url := "http://www.meteofrance.com/mf3-rpc-portlet/js/datas/bulletins_marine.json"
-	groups := map[string][]AreaGroup{}
-	err := getJson(url, &groups)
+func httpGet(url string, headers map[string]string) (io.ReadCloser, error) {
+	rq, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return nil, err
 	}
-	group, ok := groups["bulletinsMarineMetropole"]
-	if !ok {
-		return nil, fmt.Errorf("cannot extract metropole areas")
+	for k, v := range headers {
+		rq.Header.Set(k, v)
 	}
-	areas := []CoastalArea{}
-	for _, g := range group {
-		if g.Name != "cote" {
-			continue
-		}
-		for _, a := range g.Areas {
-			areas = append(areas, CoastalArea{
-				Id:  a.Id,
-				URL: a.URL,
-			})
-		}
+	rq.Header.Set("User-Agent", "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)")
+	rsp, err := http.DefaultClient.Do(rq)
+	if err != nil {
+		return nil, err
 	}
-	return areas, nil
+	if rsp.StatusCode != http.StatusOK {
+		rsp.Body.Close()
+		return nil, fmt.Errorf("got %d fetching %s", rsp.StatusCode, url)
+	}
+	return rsp.Body, nil
+}
+
+func htmlGet(url string, headers map[string]string, enc encoding.Encoding) (
+	*goquery.Document, error) {
+
+	r, err := httpGet(url, headers)
+	if err != nil {
+		return nil, err
+	}
+	defer r.Close()
+	var reader io.Reader = r
+	if enc != nil {
+		reader = transform.NewReader(reader, enc.NewDecoder())
+	}
+	//reader = &LogReader{r}
+	return goquery.NewDocumentFromReader(reader)
+}
+
+type Forecast struct {
+	Id      string
+	Title   string
+	Content string
+}
+
+func htmlToText(spans *goquery.Selection) string {
+	var buf bytes.Buffer
+
+	for i := range spans.Nodes {
+		span := spans.Eq(i)
+		class, _ := span.Attr("class")
+		if class == "titre2" || class == "titre3" {
+			buf.WriteString("\n")
+		}
+		buf.WriteString(strings.TrimSpace(span.Text()) + "\n")
+	}
+	return buf.String()
+}
+
+func parseWeather(doc *goquery.Document) []Forecast {
+	forecasts := []Forecast{}
+	zones := doc.Find("div[class='affichebulletins']")
+	for i := range zones.Nodes {
+		zone := zones.Eq(i)
+		id, _ := zone.Attr("id")
+		title := zone.Find("h2").First().Text()
+		contentSel := zone.Find("span")
+		content := htmlToText(contentSel)
+		forecasts = append(forecasts, Forecast{
+			Id:      id,
+			Title:   title,
+			Content: content,
+		})
+	}
+	return forecasts
+}
+
+func fetchForecasts() ([]Forecast, error) {
+	url := "http://vigiprevi.meteofrance.com/data/VBFR02_LFPW_.txt"
+	headers := map[string]string{}
+	doc, err := htmlGet(url, headers, nil)
+	if err != nil {
+		return nil, err
+	}
+	forecasts := parseWeather(doc)
+	if len(forecasts) <= 0 {
+		return nil, fmt.Errorf("no forecast found")
+	}
+	return forecasts, nil
 }
 
 const (
-	html = `<html>
+	htmlTemplate = `<html>
 <head>
-	<title>Gale warning number evolution in Brest area</title>
+	<title>Marine weather forecasts in Brest area</title>
 </head>
 <body>
 	{{range .}}
@@ -179,16 +125,16 @@ const (
 `
 )
 
-func formatAreas(t *template.Template, areas []CoastalArea) (string, error) {
+func formatAreas(t *template.Template, forecasts []Forecast) (string, error) {
 	type Area struct {
 		URL  string
 		Name string
 	}
 	data := []Area{}
-	for _, area := range areas {
+	for _, forecast := range forecasts {
 		data = append(data, Area{
-			URL:  "areas/" + area.Id,
-			Name: area.URL,
+			URL:  "areas/" + forecast.Id,
+			Name: forecast.Title,
 		})
 	}
 	w := &bytes.Buffer{}
@@ -200,14 +146,14 @@ func formatAreas(t *template.Template, areas []CoastalArea) (string, error) {
 }
 
 func renderAreas(t *template.Template) (string, error) {
-	areas, err := fetchAreas()
+	forecasts, err := fetchForecasts()
 	if err != nil {
 		return "", err
 	}
-	return formatAreas(t, areas)
+	return formatAreas(t, forecasts)
 }
 
-func formatJsonAreas(t *template.Template, w http.ResponseWriter, req *http.Request) {
+func serveAreas(t *template.Template, w http.ResponseWriter, req *http.Request) {
 	areas, err := renderAreas(t)
 	if err != nil {
 		w.Header().Set("Content-Type", "text/plain;charset=utf-8")
@@ -226,6 +172,43 @@ func formatJsonAreas(t *template.Template, w http.ResponseWriter, req *http.Requ
 	fmt.Fprintf(w, "%s", areas)
 }
 
+func renderForecast(id string) (string, error) {
+	forecasts, err := fetchForecasts()
+	if err != nil {
+		return "", err
+	}
+	forecast := Forecast{}
+	for _, f := range forecasts {
+		if f.Id == id {
+			forecast = f
+			break
+		}
+	}
+	if forecast.Id == "" {
+		return "", fmt.Errorf("cannot find forecast: %s", id)
+	}
+	return forecast.Content, nil
+}
+
+func serveForecast(w http.ResponseWriter, req *http.Request) {
+	id := path.Base(req.URL.Path)
+	report, err := renderForecast(id)
+	w.Header().Set("Content-Type", "text/plain;charset=utf-8")
+	if err != nil {
+		w.WriteHeader(500)
+		fmt.Fprintf(w, "error: %s\n", err)
+		return
+	}
+	h := hashReport(report)
+	w.Header().Set("ETag", h)
+	etag := req.Header.Get("If-None-Match")
+	if etag == h {
+		w.WriteHeader(304)
+		return
+	}
+	fmt.Fprintf(w, "%s", report)
+}
+
 var (
 	serveCmd    = app.Command("serve", "reformat forecasts and serve them over HTTP")
 	servePrefix = serveCmd.Flag("prefix", "public URL prefix").String()
@@ -235,15 +218,15 @@ var (
 func serveFn() error {
 	prefix := *servePrefix
 	addr := *serveHttp
-	t, err := template.New("areas").Parse(html)
+	t, err := template.New("areas").Parse(htmlTemplate)
 	if err != nil {
 		return err
 	}
 	mux := http.NewServeMux()
 	mux.HandleFunc(prefix+"/", func(w http.ResponseWriter, req *http.Request) {
-		formatJsonAreas(t, w, req)
+		serveAreas(t, w, req)
 	})
-	mux.HandleFunc(prefix+"/areas/", formatJsonWeather)
+	mux.HandleFunc(prefix+"/areas/", serveForecast)
 	fmt.Printf("serving on %s\n", addr)
 	return http.ListenAndServe(addr, httpgzip.NewHandler(mux))
 }
